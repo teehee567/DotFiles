a016d46177a466cbb7d8d4873b272838afa31a6b
Alex <81747218+teehee567@users.noreply.github.com>
2024-06-23 16:22:38 +1000

    flasdjkf

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -24,6 +24,9 @@ vim.keymap.set('n', "<C-Right>", ":vertical resize -2<CR>", { silent = true })
 -- Toggle tree
 vim.keymap.set('n', '<leader>e', "<cmd>NvimTreeToggle<CR>", { desc = "toggle tree explorer"})
 
+-- yank all
+vim.keymap.set('n', '<leader>y', "<cmd>%y<CR>", { desc = "yank all"})
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
